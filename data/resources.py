cd2c0634626002b159c5c0be90b26be77687958c
PixelLover <41162341+TJay12@users.noreply.github.com>
2025-05-22 11:01:13 +1200

    Convert data files from txt to json commit

--- data/resources.py
+++
@@ -1,9 +0,0 @@
-# Resources Dictionary--------------------------------------------------------------------------------------------------
-resources = {
-    # "plains": {
-        "berry bush": {"berries"},
-        "herb patch": {"medicinal herbs"},
-        "boulder": {"stones"},
-        "tree": {"twigs"}
-    # }
-}
